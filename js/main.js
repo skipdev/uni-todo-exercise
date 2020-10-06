3842bcf66a694756b737ee3f7e0eef8f898f8f75
Skip <skip.barden@gmail.com>
2020-10-06 10:16:55 +0100

    complete all tasks in the todo exercise

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-import todos from "./todos";
+import todoList from "./todos";
 import squareNumber from "./squareNumber";
 import "../main.scss";
 import img1 from "../assets/top-left-elips.png";
 import img2 from "../assets/bottom-right-elips.png";
+import Todo from './Components/Todo';
 
-console.log(todos);
+console.log(todoList);
 console.log(squareNumber(9));
 
 const view = `
@@ -23,8 +24,19 @@ const view = `
 </div>
 <!-- /.wrapper -->`;
 
-const render = () => {
+const render = (todos) => {
     console.log('ready');
     document.getElementById('app').innerHTML = view;
+
+    const todosCopy = [...todos];
+    const handleCloseClick = (e) => {
+        console.log('close')
+        render(todosCopy.filter((c) => e.target.dataset.outerItem !== c.id));
+    };
+
+    document.querySelector("#app").innerHTML = view;
+    let htmlList = todos.map((item) => Todo(item.text, item.id));
+    document.querySelector(".list").innerHTML = htmlList.join('');
+    document.querySelectorAll(".close").forEach((e) => e.addEventListener("click", handleCloseClick));
 }
-window.addEventListener('DOMContentLoaded', render);
+window.addEventListener('DOMContentLoaded', render(todoList));
--- js/todos.js
+++ js/todos.js
@@ -1,13 +1,19 @@
 const todos = [
     {
         id: "1",
-        text: "Complete My Assessment",
+        text: "Complete My Assessment 1",
         created: "Wed Jan 22 2020 07:02:0",
         completed: false,
     },
     {
         id: "2",
-        text: "Complete My Assessment",
+        text: "Complete My Assessment 2",
+        created: "Wed Jan 22 2020 07:03:0",
+        completed: false,
+    },
+    {
+        id: "3",
+        text: "Complete My Assessment 3",
         created: "Wed Jan 22 2020 07:03:0",
         completed: false,
     },
